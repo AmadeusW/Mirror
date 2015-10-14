df1460eddf74406d9a97b04c7969366ff129b917
Amadeus Wieczorek <git@amadeusw.com>
2015-10-14 16:59:14 -0700

    Created TimerController

---
+++ src/AmadeusW.Mirror.GUI/Controllers/TimerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace AmadeusW.Mirror.GUI.Controllers
+{
+    static class TimerController
+    {
+        public static void RegisterModel(BaseModel model)
+        {
+            var timer = new DispatcherTimer();
+            timer.Interval = model.Interval;
+            timer.Tick += model.TimerTick;
+            timer.Start();
+        }
+    }
+}
