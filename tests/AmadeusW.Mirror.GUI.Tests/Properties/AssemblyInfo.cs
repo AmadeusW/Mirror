6a78acdb1de6b8223dc177d09f72ed76252b89b4
Amadeus Wieczorek <git@amadeusw.com>
2015-11-02 09:02:10 -0800

    Adds the test project

--- tests/AmadeusW.Mirror.GUI.Tests/Properties/AssemblyInfo.cs
+++ tests/AmadeusW.Mirror.GUI.Tests/Properties/AssemblyInfo.cs
@@ -5,14 +5,15 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AmadeusW.Mirror.GUI")]
+[assembly: AssemblyTitle("AmadeusW.Mirror.GUI.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AmadeusW.Mirror.GUI")]
+[assembly: AssemblyProduct("AmadeusW.Mirror.GUI.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyMetadata("TargetPlatform","UAP")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -27,4 +28,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
-[assembly: InternalsVisibleTo("AmadeusW.Mirror.GUI.Tests")]
