f738e12aa0135b4e7dc235ee4747a46fd3346530
Amadeusz Wieczorek <git@amadeusw.com>
2016-03-06 19:16:28 -0800

    begins to change the transit view model

--- src/AmadeusW.Mirror.GUI/Transit/ArrivalViewModel.cs
+++ src/AmadeusW.Mirror.GUI/Transit/ArrivalViewModel.cs
@@ -4,6 +4,7 @@ namespace AmadeusW.Mirror.GUI.Transit
 {
     public class ArrivalViewModel
     {
+        public string RouteName { get; set; }
         public int WhenINeedToLeave { get; set; }
         public DateTime ArrivalTime { get; set; }
     }
